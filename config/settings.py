bf0a1159969946af8f0f61c51de7d74217ea2d5a
Islom <114618345+Axiy2001@users.noreply.github.com>
2025-11-30 16:48:53 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'staticfiles',
     }
 }
 
@@ -121,3 +121,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
